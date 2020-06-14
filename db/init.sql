1c99c36762d361bcc0dc68c6dc44d448c9b0af0a
Christian Bamatembera <chrisbama92@gmail.com>
2020-06-13 22:21:42 -0400

    Added remaining schemas to init sql script

--- db/init.sql
+++ db/init.sql
@@ -7,5 +7,36 @@ CREATE TABLE user (
   Email VARCHAR(255) NOT NULL,
   Password VARCHAR(255) NOT NULL,
   TotalScore SMALLINT,
+  BestScore SMALLINT,
   CONSTRAINT UC_Person UNIQUE(ID, UserName, Email)
+);
+
+CREATE TABLE category (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	Name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE question (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	Description VARCHAR(255) NOT NULL,
+	-- tf: true/false, mc: multiple choice, sa: short answer
+	Type ENUM('tf', 'mc', 'sa'),
+	ChoiceA VARCHAR(255),
+	ChoiceB VARCHAR(255),
+	ChoiceC VARCHAR(255),
+	ChoiceD VARCHAR(255),
+	Answer VARCHAR(255) NOT NULL,
+	CategoryID INT NOT NULL,
+	Difficulty VARCHAR(255) NOT NULL,
+	Points INT NOT NULL,
+	FOREIGN KEY (CategoryID) REFERENCES category(ID)
+);
+
+CREATE TABLE leaderboard (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	UserID INT NOT NULL,
+	CategoryID INT NOT NULL,
+	Score SMALLINT,
+	FOREIGN KEY (UserID) REFERENCES user(ID),
+	FOREIGN KEY (CategoryID) REFERENCES category(ID)
 );
